fb63ee06aabdfe178ceaf103df54ed2d922f1641
wageeshar <wageesha.rcc07@gmail.com>
2023-12-29 14:54:47 +0000

    Add .env loading, setup api url

---
+++ vite-env.d.ts
@@ -0,0 +1,2 @@
+export const __APP_VERSION__: string
+export const __API_URL__: string
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command, mode}) => {
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: loadEnv(mode, process.cwd(), ''),
+      __APP_VERSION__: JSON.stringify('v1.0.0')
+    }
+  }
 })
